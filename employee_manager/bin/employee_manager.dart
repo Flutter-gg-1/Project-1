2117eddde2211b85389c3ea8d205d643643aa0c3
Yaser Alkhayyat <yaserkhayyat2017@gmail.com>
2024-08-01 21:22:02 +0300

    add managerMenu function

--- employee_manager/bin/employee_manager.dart
+++ employee_manager/bin/employee_manager.dart
@@ -1,3 +1,34 @@
+// imports
+import 'dart:io';             // library to interact with user
+import 'manager_menu.dart';   // function to print manager's menu
+
 void main() {
-  print("Employee manager app");
+  // initialize list of employees
+  List employees = [];
+  while_manage:
+  while(true) {
+    managerMenu();
+    stdout.write("Choose an option : ");
+    String? action = stdin.readLineSync();
+    switch(action) {
+      case '0' :
+      print("good bye ...");
+      break while_manage;
+
+      case '1' :
+      // view employees
+
+      case '2' :
+      // add new employee
+
+      case '3' :
+      // modify employee info
+
+      case '4' :
+      // remove employee
+
+      default :
+      print("ERROR !! : Invalid choice\n");
+    }
+  }
 }
---
+++ employee_manager/bin/manager_menu.dart
@@ -0,0 +1,15 @@
+void managerMenu() {
+  print('='*30);
+  print('|   Employee  Manager  App   |');
+  print('='*30);
+  print('| 1 |     View Employees     |');
+  print('-'*30);
+  print('| 2 |   Add a New Employee   |');
+  print('-'*30);
+  print('| 3 |  Modify Employee Info  |');
+  print('-'*30);
+  print('| 4 |     Remove Employee    |');
+  print('-'*30);
+  print('| 0 |          EXIT          |');
+  print('='*30);
+}
