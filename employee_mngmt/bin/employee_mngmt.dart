fae03406d45f334ba6e183cc519e8c2f194297d1
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-08-01 20:57:46 +0300

    created dart project emplloyee_mngmt

---
+++ employee_mngmt/bin/employee_mngmt.dart
@@ -0,0 +1,5 @@
+import 'package:employee_mngmt/employee_mngmt.dart' as employee_mngmt;
+
+void main(List<String> arguments) {
+  print('Hello world: ${employee_mngmt.calculate()}!');
+}
---
+++ employee_mngmt/test/employee_mngmt_test.dart
@@ -0,0 +1,8 @@
+import 'package:employee_mngmt/employee_mngmt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
