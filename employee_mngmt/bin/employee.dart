2fdffe9a4efbc0989ca6152a1e6966925ef17ea2
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-08-01 21:20:41 +0300

    added Employee class with generateEmployeeID()

---
+++ employee_mngmt/bin/employee.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+
+class Employee {
+  String? firstName;
+  String? lastName;
+  String? empID;
+  String? nationality;
+  String? dateOfBirth;
+  String? gender;
+
+  String? phoneNumber;
+  String? email;
+  String? address;
+
+  String? role;
+  String? jobDescription;
+  String? dept;
+  int? salary;
+
+  Employee(
+      {required this.firstName,
+      required this.lastName,
+      required this.role,
+      this.nationality,
+      this.dateOfBirth,
+      this.gender,
+      this.phoneNumber,
+      this.email,
+      this.address,
+      this.jobDescription,
+      this.dept,
+      this.salary}) {
+    empID = generateEmployeeID();
+  }
+
+  String generateEmployeeID() {
+    Random random = Random();
+
+    int firstDigit = random.nextInt(9) + 1; //Start with number other than 0
+    String remainingDigits =
+        List.generate(4, (generate) => random.nextInt(10)).join();
+
+    return '$firstDigit$remainingDigits';
+  }
+}
