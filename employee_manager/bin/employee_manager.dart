0ab569a323d34e25329839cfa756002dfb7adce4
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-02 10:10:45 +0300

    frist App

--- employee_manager/bin/employee_manager.dart
+++ employee_manager/bin/employee_manager.dart
@@ -1,5 +1,4 @@
-import 'package:employee_manager/employee_manager.dart' as employee_manager;
 
-void main(List<String> arguments) {
-  print('Hello world: ${employee_manager.calculate()}!');
+void main() {
+  print('Hello, World!');
 }
