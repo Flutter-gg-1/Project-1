7f863349ed9477c12683a78328e79d8fcccbc56c
Basel-75 <basel_1422@outlook.com>
2024-08-02 03:17:38 +0300

    Create : dart project'employee_manger_app'

---
+++ employee_manager_app/bin/employee_manager_app.dart
@@ -0,0 +1,5 @@
+import 'package:employee_manager_app/employee_manager_app.dart' as employee_manager_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${employee_manager_app.calculate()}!');
+}
---
+++ employee_manager_app/test/employee_manager_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:employee_manager_app/employee_manager_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
