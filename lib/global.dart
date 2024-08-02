a824e7002944dcad612401a594419ab62ded6be4
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-02 17:39:50 +0300

    create defult admin for the system

--- lib/global.dart
+++ lib/global.dart
@@ -1,5 +1,11 @@
 import "employee.dart";
 
-late List<Employee> emps = [];
+Employee admin = Employee(
+    id: 0,
+    name: "admin",
+    salary: 0,
+    jobDescriptions: "admin of the system",
+    permission: PermissionLevel.admin.toString());
+List<Employee> emps = [admin];
 
 enum PermissionLevel { admin, dev, it }
