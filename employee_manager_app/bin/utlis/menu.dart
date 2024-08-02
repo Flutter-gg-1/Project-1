8fcb176c5a319031df072d0c01c2701cf7747785
Basel-75 <basel_1422@outlook.com>
2024-08-02 03:32:13 +0300

    Create : class Menu

---
+++ employee_manager_app/bin/utlis/menu.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+class Menu{
+  static menu(){
+    String input = stdin.readLineSync().toString().trim();
+    
+    switch (input) {
+      case '1':
+        
+        break;
+      case '2':
+        
+        break;
+      case '3':
+        
+        break;
+      case '4':
+        
+        break;
+      case '5':
+        
+        break;
+      case '0':
+        exit(0);
+      default:
+    }
+  }
+}
--- employee_manager_app/test/employee_manager_app_test.dart
+++ employee_manager_app/test/employee_manager_app_test.dart
@@ -1,8 +1,4 @@
-import 'package:employee_manager_app/employee_manager_app.dart';
-import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+
+  }
