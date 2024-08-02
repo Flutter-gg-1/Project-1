faa2a5cc3eaf9b7f4af2a975e994d1f4e2eb1a82
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-02 16:15:34 +0300

    ADD Employee

--- employee_manager/bin/all_file.dart
+++ employee_manager/bin/all_file.dart
@@ -1,3 +1,5 @@
 export './emp_manager.dart';
 export './supervisor_employee.dart';
 export './workers.dart';
+export './main_dashboard.dart';
+export './employee_manager.dart';
--- employee_manager/bin/emp_manager.dart
+++ employee_manager/bin/emp_manager.dart
@@ -1,5 +1,26 @@
 // Employee Manager
 import 'dart:io';
+import 'all_file.dart';
 void employeeManager(){
-  print('Employee Manager');
+  print('Dashboard of Employee Manager');
+  print('1. Manage Employee  2. Supervisor');
+  print('3. Workers   4. Main Dashboard');
+  stdout.write('Enter your choice: ');
+  int? choice = int.parse(stdin.readLineSync()!);
+  switch(choice){
+    case 1:
+      employeeManager();
+      break;
+    case 2:
+      supervisorEmployee();
+      break;
+    case 3:
+      workers();
+      break;
+    case 4:
+      mainDashboard();
+      break;
+    default:
+      print('Invalid choice');
+  }
 }
