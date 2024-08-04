90ecc2167f7027b662927a4c44ad05f987a38b21
MAHA Al-ATAWI <mem880.a@gmail.com>
2024-08-04 08:49:19 +0300

    Adding main files

--- project_01/bin/project_01.dart
+++ project_01/bin/project_01.dart
@@ -1,3 +1,36 @@
-void main(){
-  
+import 'dart:io';
+
+void main() {
+  while (true) {
+    print("\nEmployee Manager");
+    print("1. Add New Employee");
+    print("2. Assign Permissions");
+    print("3. Display Employee Data");
+    print("4. Modify Salary");
+    print("5. Modify Job Description");
+    print("6. List All Employees");
+    print("0. Exit");
+
+    String? choice = stdin.readLineSync();
+
+    switch (choice) {
+      case "1":
+        break;
+      case "2":
+        break;
+      case "3":
+        break;
+      case "4":
+        break;
+      case "5":
+        break;
+      case "6":
+        break;
+      case "0":
+        print("Goodbye!");
+        return;
+      default:
+        print("Invalid choice. Please try again.");
+    }
+  }
 }
