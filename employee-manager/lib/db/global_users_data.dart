ff2101e5883832991006ea2d05762f8fb3c5dd47
FaHaDoF69 <93477293+FaHaDoF69@users.noreply.github.com>
2024-08-04 08:39:05 +0300

    add methods to edit and check permission of the user, add global var to save user data and makethe control easy

---
+++ employee-manager/lib/db/global_users_data.dart
@@ -0,0 +1,3 @@
+/// Global var for store users data
+/// TODO delete it when you finish
+List<Map<String, dynamic>> globalUsersData = [];
--- employee-manager/lib/src/user.dart
+++ employee-manager/lib/src/user.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:employee_manager/helpers/generate_random_id.dart';
 
+import '../db/global_users_data.dart';
 import '../enum/user_permission.dart';
 
 /*
@@ -21,6 +24,53 @@ class User {
     id = generateRandomId();
   }
 
+  /*
+  *
+  * User Sections
+  *
+  * */
+  // Method to edit user data // TODO write a comments
+  void updateUser({required String id}) {
+    Map<String, dynamic> usersAfterUpdate = {};
+    int count = 0;
+    for (var element in globalUsersData) {
+      if (element["id"] == id) {
+        usersAfterUpdate = element;
+        break;
+      }
+      count++;
+    }
+
+    if (usersAfterUpdate.isNotEmpty) {
+      print("-----ID User ${usersAfterUpdate["id"]}");
+      print("Name: ${usersAfterUpdate["name"]}");
+      print("Password: ${usersAfterUpdate["password"]}");
+      print("Permission: ${usersAfterUpdate["permission"]}");
+      print("-------------------------------");
+      print('\n');
+      print("Edit his new name");
+      String? newName = stdin.readLineSync();
+
+      print("Edit his new password");
+      String? newPassword = stdin.readLineSync();
+
+      String? newPermission;
+      // Check user input if a permission is correct
+      do {
+        print("Edit his new permission (admin, manager, employee)");
+        newPermission = stdin.readLineSync().toString();
+      } while (!isPermission(permission: newPermission));
+
+      // Update values
+      usersAfterUpdate["name"] = newName;
+      usersAfterUpdate["password"] = newPassword;
+      usersAfterUpdate["permission"] = newPermission;
+      globalUsersData[count] = usersAfterUpdate;
+    } else {
+      print("-----ID user $id not found");
+    }
+  }
+
   // Converts the User object to a JSON-compatible format
   Map<String, dynamic> toJson() {
     return {
@@ -36,7 +86,7 @@ class User {
 
   /*
   *
-  * Method for permissions check
+  * Permission Sections
   *
   * */
   // Method to check if user has admin permission
@@ -52,5 +102,14 @@ class User {
   // Method to check if user has employee permission
   bool isEmployee() {
     return permission == UserPermission.employee;
+  }
+
+  // Method to check if permission in enum or not
+  bool isPermission({required String permission}) {
+    // Convert enum to list
+    var allPermission = UserPermission.values.map((e) => e.name).toList();
+
+    // Return true when user input is a correct permission
+    return allPermission.contains(permission);
   }
 }
